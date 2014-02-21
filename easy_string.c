79b16bd5e9f529806def4ba2425e8f661617dd0d
Nathan West <Lucretiel@gmail.com>
2014-02-21 14:34:26 -0500

    Major updates all over the place. It's coming together

--- easy_string.c
+++ easy_string.c
@@ -10,27 +10,45 @@
 #include <string.h>
 #include "easy_string.h"
 
-//True if the string is using the shortstring optimization
-const static inline bool shortstring_optimized(const String* str)
+static inline bool shortstr(size_t size)
 {
-	return str->size < sizeof(str->shortstr);
+	return size <= shortstr_max;
 }
 
-//True if the string owns to allocated memory
-static inline bool allocated(const String* str)
+static inline bool shortstr_optimized(const String* str)
 {
-	return !shortstring_optimized(str) && str->owns;
+	return shortstr(str->size);
 }
 
-//If the size is too long for a shortstring, allocate. Also set the size.
+/*
+ * Get the total amount of allocated memory, including potential space before
+ * begin. Returns a size only if the string owns allocated memory, otherwise
+ * returns 0.
+ */
+static inline size_t allocated_size(const String* str)
+{
+	return shortstr_optimized(str) ?
+		0 : str->alloc_begin ?
+			str->alloc_end - str->alloc_begin :
+			0;
+}
+
+/*
+ * Force str to have at least size bytes available, possibly with a malloc.
+ * This is an unsafe function; make sure to set everything else correctly
+ * beforehand (freeing pointers, etc).
+ *
+ * Note that a side-effect of this design is that a string of
+ * size < shortstr_max can't have allocated memory. This is fine.
+ */
 static inline void autoalloc(String* str, size_t size)
 {
 	str->size = size;
-
-	if(!shortstring_optimized(str))
+	if(!shortstr(size))
 	{
-		str->begin = calloc(size, 1);
-		str->owns = true;
+		str->alloc_begin = malloc(size);
+		str->alloc_end = str->alloc_begin + size;
+		str->begin = str->alloc_begin;
 	}
 }
 
@@ -44,11 +62,29 @@ const char* string_cstrc(const String* str)
 {
 	return STRING_CSTR(str);
 }
+#undef STRING_CSTR
+
+size_t string_size(const String* str)
+{
+	return str->size;
+}
+
+size_t string_available(const String* str)
+{
+	return shortstr_optimized(str) ?
+		sizeof(str->shortstr) :
+		str->alloc_end - str->begin;
+}
+
+int string_owns(const String* str)
+{
+	return shortstr_optimized(str) || str->alloc_begin;
+}
 
 void string_free(String* str)
 {
-	if(allocated(str))
-		free(str->begin);
+	if(allocated_size(str))
+		free(str->alloc_begin);
 }
 
 String string_copy_cstr(const char* str)
@@ -66,11 +102,14 @@ String string_move_cstr(char* str)
 { return string_moven_cstr(str, strlen(str)); }
 
 String string_moven_cstr(char* str, size_t size)
+{ return string_movena_cstr(str, size, size); }
+
+String string_movena_cstr(char* str, size_t size, size_t alloc)
 {
 	String result = empty_string;
 	result.size = size;
 
-	if(shortstring_optimized(&result))
+	if(shortstr(size))
 	{
 		memcpy(result.shortstr, str, size);
 		free(str);
@@ -78,7 +117,8 @@ String string_moven_cstr(char* str, size_t size)
 	else
 	{
 		result.begin = str;
-		result.owns = 1;
+		result.alloc_begin = str;
+		result.alloc_end = str + alloc;
 	}
 	return result;
 }
@@ -91,22 +131,21 @@ String string_tempn_cstr(char* str, size_t size)
 	String result = empty_string;
 	result.size = size;
 
-	if(shortstring_optimized(&result))
+	if(shortstr(size))
 	{
 		memcpy(result.shortstr, str, size);
 	}
 	else
 	{
 		result.begin = str;
+		result.alloc_end = str + size;
 	}
 
 	return result;
 }
 
 String string_copy(const String* str)
-{
-	return string_copyn_cstr(string_cstrc(str), str->size);
-}
+{ return string_copyn_cstr(string_cstrc(str), str->size); }
 
 String string_move(String* str)
 {
@@ -123,30 +162,118 @@ String string_temp(String* str)
 	String result = *str;
 	if(!shortstring_optimized(str))
 	{
-		result.owns = 0;
+		result.alloc_begin = 0;
 	}
+	return result;
+}
+
+String string_reserved(size_t size)
+{
+	String result = empty_string;
+	autoalloc(&result, size);
 	return result;
 }
 
 String string_concat(String str1, String str2)
 {
 	String result = empty_string;
-	autoalloc(&result, str1.size + str2.size);
-	char* dest = string_cstr(&result);
-	memcpy(dest, string_cstr(&str1), str1.size);
-	memcpy(dest + str1.size, string_cstr(&str2), str2.size);
+	result.size = string_size(&str1) + string_size(&str2);
+	//Attempt to just do buffer pointer shifts.
+
+	/*
+	 * We can't do a pointer shift if the 2 string refer to the same memory, and
+	 * both have an offset. We can only do it if str2 owns its data, or str1
+	 * doesn't have an offset, or if str2 happens to be at the correct offset.
+	 */
+	if(allocated_size(&str1) >= result.size &&
+		(string_owns(&str2) ||
+			str1.begin == str1.alloc_begin ||
+			str2.begin == str1.alloc_begin + str1.size ))
+	{
+		//Give str1's buffer to result
+		result.alloc_begin = str1.alloc_begin;
+		result.alloc_end = str1.alloc_end;
+		result.begin = result.alloc_begin;
+
+		//Shift the memory to the beginning of the allocated space, if needed
+		memmove(result.alloc_begin, str1.begin, str1.size);
+
+		//Copy str2 to the array
+		memcpy(result.alloc_begin + str1.size, string_cstrc(&str2), str2.size);
+
+		//Wipe str1's pointers so it isn't free'd
+		str1.alloc_begin = 0;
+	}
+	/*
+	 * Same logic and above. Need to be sure it's safe to memmove and memcpy.
+	 */
+	else if(allocated_size(&str2) >= result.size &&
+		(string_owns(&str1) ||
+			str1.begin == str2.alloc_begin ||
+			str2.begin == str2.alloc_begin + str1.size))
+	{
+		//Give str2's buffer to result
+		result.alloc_begin = str2.alloc_begin;
+		result.alloc_end = str2.alloc_end;
+		result.begin = result.alloc_begin;
+
+		//Shift str2's memory over, to make room for str1
+		memmove(result.alloc_begin + str1.size, str2.begin, str2.size);
+
+		//Copy str1 to the array
+		memcpy(result.alloc_begin, string_cstrc(&str1), str1.size);
+
+		//Wipe str2's pointers so it isn't free'd
+		str2.alloc_begin = 0;
+	}
+	else
+	{
+		/*
+		 * Allocate new memory, copy both strings. Can't use raw underlying
+		 * pointers because this may be a shortstring.
+		 */
+		autoalloc(&result, result.size);
+		char* dest = string_cstr(&result);
+		memcpy(dest, string_cstr(&str1), str1.size);
+		memcpy(dest + str1.size, string_cstr(&str2), str2.size);
+	}
 	string_free(&str1);
 	string_free(&str2);
 	return result;
 }
 
-String string_substr(String* str, size_t offset, size_t length)
+void string_append(String* dest, String source)
 {
-	size_t actual_length = offset > str->size ?
-		0 : offset + length > str->size ?
-			str->size - offset : length;
+	*dest = string_concat(string_move(dest), source);
+}
 
-	return string_tempn_cstr(string_cstr(str), actual_length);
+#define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
+
+String string_substr(String str, size_t offset, size_t length)
+{
+	String result;
+
+	if(offset < string_size(str))
+	{
+		result.size = MIN(length, string_size(str) - offset);
+		if(shortstr(length))
+		{
+			memcpy(result.shortstr, string_cstrc(&str), result.size);
+		}
+		else
+		{
+			result.begin = string_cstr(&str) + result.size;
+			result.alloc_begin = str.alloc_begin;
+			result.alloc_end = str.alloc_end;
+			str.alloc_begin = 0;
+		}
+	}
+	else
+	{
+		result = empty_string;
+	}
+	string_free(str);
+	return result;
 }
 
 int string_compare(String str1, String str2)
@@ -158,8 +285,6 @@ int string_compare(String str1, String str2)
 	string_free(&str2);
 	return result;
 }
-
-static inline int min(int x, int y) { return x < y ? x : y; }
 
 int string_prefix(String str1, String str2)
 {
--- easy_string.h
+++ easy_string.h
@@ -18,11 +18,62 @@
  *  string; Care should be taken to ensure they don't outlive their source.
  *
  *  In general, it should be assumed that stack-scoped String objects should be
- *  cleared, no matter what, before going out of scope.
+ *  clear'd or string_free'd, no matter what, before going out of scope.
+ *
+ *  The basic design is trying to emulate, as much as possible, the C++
+ *  reference model. There are 3 kinds of constructor- copy, move, and temp.
+ *  Copy constructors full copy the string and make a new one. move constructors
+ *  transfer the ownership of allocated memory to the new string, nulling the
+ *  pointers of the old one. temp constructors create tempstrings, which share
+ *  data pointers with their source but don't own the string. These
+ *  constructors have both String and char* versions.
+ *
+ *
+ *  Implementation details:
+ *
+ *  There are essentially 3 kinds of strings to think about- shortstrings,
+ *  regular strings, and tempstrings. They have similar but not identical
+ *  behaviors over all these operations, though the library is designed so that
+ *  these details are unimportant.
+ *
+ *  shortstrings are basically any string with less than sizeof(char*) * 3
+ *  bytes. Because allocated strings need 3 pointers to keep track of the
+ *  memory, we can just cram all the bytes in that space if its small enough.
+ *  shortstrings always have pure copy semantics, even when moving or temping.
+ *  They always have the whole space available. They are automatically created
+ *  if the size is small enough.
+ *
+ *  regular strings have owned, allocated memory. They are distinguished from
+ *  tempstrings by having a non-null alloc pointer. They are considered to own
+ *  their memory; when cleared, the memory is freed. They copy their data on
+ *  a copy, transfer ownership on a copy, and send the pointer on a temp.
+ *
+ *  tempstrings don't own their memory, and are distinguished by having null
+ *  alloc pointers. They create full, owning copies when copied, but create
+ *  other tempstrings when moved or temped.
+ *
+ *  Note that the design is such that a string can own memory but only use an
+ *  offsetted subset of that memory. This is to allow a hypothetical future
+ *  design that allows for owning substrings (potentially with a move)
  */
 
 #pragma once
 
+/*
+ * Description of struct:
+ *
+ * OI: CLIENTS. Use the string_cstr, string_cstrc, string_size, and
+ * string_available instead of accessing the struct directly.
+ *
+ * size: the actual size of the string
+ * begin: the beginning of the useful data in the string
+ * alloc_begin: the alloc'd pointer. NULL if this string doesn't own the memory
+ * alloc_end: the end of the allocated memory. This is set even if the string
+ *   doesn't actually own the memory.
+ * shortstr: union'd against the pointers above, this allows short strings to
+ * just use the bytes that would be filled with pointers, if the string is short
+ * enough. It's probably 24 bytes on a 64-bit system and 12 on a 32-bit.
+ */
 typedef struct
 {
 	size_t size;
@@ -31,9 +82,10 @@ typedef struct
 		struct
 		{
 			char* begin;
-			int owns;
+			char* alloc_begin;
+			char* alloc_end;
 		};
-		char shortstr[2 * sizeof(char*)];
+		char shortstr[3 * sizeof(char*)];
 	};
 } String;
 
@@ -41,16 +93,44 @@ typedef struct
 char* string_cstr(String* str);
 const char* string_cstrc(const String* str);
 
+/*
+ * Actual size of a string. Pass this along with a char* to other functions.
+ */
+size_t string_size(const String* str);
+
+/*
+ * Maximum hypothetical size of a string. This is how many bytes are available
+ * to write. Feel free to mutate, using the string_cstr pointer, but make sure
+ * you update size appropriately, and be aware that other strings MAY refer to
+ * this memory.
+ */
+size_t string_avalable(const String* str);
+
+/*
+ * True if the string owns its memory. This means that when the string is
+ * clear'd, the memory will become invalid; any tempstrings (possibly including
+ * substrings) will become invalid.
+ */
+int string_owns(const String* str);
+
 //Empty string to initialize to.
 const static String empty_string;
 
-//Free the string's resources
+//I'd rather not expose this, but it's useful for clients to know
+//Largest size a shortstring can be
+const static size_t shortstr_max = sizeof(empty_string.shortstr);
+
+/*
+ * Free the string's resources. Leaves the string in an invalid state; use it
+ * before going out of scope
+ */
 void string_free(String* str);
 
 //Clear the string
 static inline void clear(String* str)
 { string_free(str); *str = empty_string; }
 
+//Construction- copy, move, temp
 //Make new string from cstring
 String string_copy_cstr(const char* str);
 String string_copyn_cstr(const char* str, size_t size);
@@ -59,7 +139,10 @@ String string_copyn_cstr(const char* str, size_t size);
 String string_move_cstr(char* str);
 String string_moven_cstr(char* str, size_t size);
 
-//Make new temp string from cstring
+//Use this to indicate a difference between the allocated size and data size
+String string_movena_cstr(char* str, size_t size, size_t alloc);
+
+//Make new temp string from cstring.
 String string_temp_cstr(char* str);
 String string_tempn_cstr(char* str, size_t size);
 
@@ -72,10 +155,30 @@ String string_move(String* str);
 //Make a temp string
 String string_temp(String* str);
 
-//Concat 2 strings
+//Make an empty string with reserved memory
+String string_reserved(size_t size);
+
+/*
+ * No assignment operators are provided- they're too complicated. Clear
+ * appropriately and use the constructors if you need assignment.
+ */
+
+/*
+ * Concatenate 2 strings. Automatically use an already allocated buffer, if
+ * possible.
+ */
 String string_concat(String str1, String str2);
 
-//Get a substring
+//Append one string to another
+void string_append(String* str1, String str2);
+
+/*
+ * Get a substring. Be careful with this- it may or may not refer to the
+ * original memory.
+ *
+ * If length is too long, the substring is automatically cut down to the
+ * correct length
+ */
 String string_substr(String* str, size_t offset, size_t length);
 
 //Compare strings
